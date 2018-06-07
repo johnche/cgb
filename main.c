47652536922a5e498796fe6e60a04379c501b619
John Chen <john20chen@gmail.com>
2018-06-07 12:10:29 +0200

    add main entry point

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "MMU.h"
+#include "Z80.h"
+
+int main(int argc, char** argv) {
+	if (argc == 2) {
+		initMMU();
+		loadCartridge(argv[1]);
+		Z80* cpu = Z80_init();
+		Z80_printCPUState(cpu);
+		return 0;
+	}
+
+	printf("Missing argument. Exiting.\n");
+	return 1;
+}
+
